3e456dcbec60d0ebb4a19baac1739037b6842362
Alex Nemeth <alex19930329@gmail.com>
2020-07-27 21:37:24 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CPUFrequencyMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelHintCpuFrequencyConst = new System.Windows.Forms.Label();
+            this.textBoxFrequency = new System.Windows.Forms.TextBox();
+            this.labelHintMhzConst = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelHintCpuFrequencyConst
+            // 
+            this.labelHintCpuFrequencyConst.AutoSize = true;
+            this.labelHintCpuFrequencyConst.Location = new System.Drawing.Point(12, 15);
+            this.labelHintCpuFrequencyConst.Name = "labelHintCpuFrequencyConst";
+            this.labelHintCpuFrequencyConst.Size = new System.Drawing.Size(82, 13);
+            this.labelHintCpuFrequencyConst.TabIndex = 1;
+            this.labelHintCpuFrequencyConst.Text = "CPU frequency:";
+            // 
+            // textBoxFrequency
+            // 
+            this.textBoxFrequency.Enabled = false;
+            this.textBoxFrequency.Location = new System.Drawing.Point(100, 12);
+            this.textBoxFrequency.MaxLength = 5;
+            this.textBoxFrequency.Name = "textBoxFrequency";
+            this.textBoxFrequency.Size = new System.Drawing.Size(57, 20);
+            this.textBoxFrequency.TabIndex = 2;
+            this.textBoxFrequency.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelHintMhzConst
+            // 
+            this.labelHintMhzConst.AutoSize = true;
+            this.labelHintMhzConst.Location = new System.Drawing.Point(163, 15);
+            this.labelHintMhzConst.Name = "labelHintMhzConst";
+            this.labelHintMhzConst.Size = new System.Drawing.Size(29, 13);
+            this.labelHintMhzConst.TabIndex = 3;
+            this.labelHintMhzConst.Text = "MHz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 43);
+            this.Controls.Add(this.labelHintMhzConst);
+            this.Controls.Add(this.textBoxFrequency);
+            this.Controls.Add(this.labelHintCpuFrequencyConst);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHintCpuFrequencyConst;
+        private System.Windows.Forms.TextBox textBoxFrequency;
+        private System.Windows.Forms.Label labelHintMhzConst;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace CPUFrequencyMonitor
+{
+    public partial class Form1 : Form
+    {
+        ManagementObjectSearcher WMIWin32_Processor;
+        DispatcherTimer CpuFrequencyCollectionTimer;
+
+        const int uiUpdateIntervalMs = 1000;
+        const int cpuFrequencyCollectionIntervalMs = 10;
+        const int accuracy = uiUpdateIntervalMs / cpuFrequencyCollectionIntervalMs;
+
+        long cpuFrequencySum;
+        int cpuFrequencyCollectionIterationCounter;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            WMIWin32_Processor = new ManagementObjectSearcher("select CurrentClockSpeed from Win32_Processor");
+
+            CpuFrequencyCollectionTimer = new DispatcherTimer();
+            CpuFrequencyCollectionTimer.Interval = new TimeSpan(0, 0, 0, 0, cpuFrequencyCollectionIntervalMs);
+            CpuFrequencyCollectionTimer.Tick += CollectCPUFrequency;
+
+            cpuFrequencySum = 0;
+            cpuFrequencyCollectionIterationCounter = 0;
+
+            CpuFrequencyCollectionTimer.Start();
+        }
+
+        private void CollectCPUFrequency(object sender, EventArgs e)
+        {
+            foreach (ManagementObject currentClockSpeedObject in WMIWin32_Processor.Get())
+            {
+                cpuFrequencySum += Convert.ToInt32(currentClockSpeedObject.GetPropertyValue("CurrentClockSpeed"));
+            }
+
+            cpuFrequencyCollectionIterationCounter++;
+
+            if (cpuFrequencyCollectionIterationCounter == accuracy)
+            {
+                textBoxFrequency.Text = (cpuFrequencySum / accuracy).ToString();
+                cpuFrequencyCollectionIterationCounter = 0;
+                cpuFrequencySum = 0;
+            }
+        }
+    }
+}
